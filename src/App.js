7d7d5ec85c0783aef86e8f2b5ed1405358405744
Asfian99 <mk.asfian@gmail.com>
2020-10-25 08:59:37 +0700

    selesai - loading screen, read more & not interested btn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import "./App.css";
 import Card from "./Card";
 import { datas } from "./datas";
 
+// const url = "./datas.json";
+
 function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  // const [datas, setDatas] = React.useState(url);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // React.useEffect(() => {
+  //   fetch(url)
+  //     .then((resp) => {
+  //       if (resp.status >= 200 && resp.status <= 299) {
+  //         console.log("hello");
+  //         setDatas(resp);
+  //         setIsLoading(false);
+  //       } else {
+  //         setIsLoading(true);
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <>
       <h1 className="title">Our Tours</h1>
--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Card(props) {
   const { img, title, price, article } = props;
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(true);
+  const [text, setText] = useState(article.substring(0, 201) + "...");
+  const [isMore, setIsMore] = useState(true);
 
   return (
-    <div className="card">
-      <img src={img} alt="" />
-      <div className="card-name">
-        <h2 className="card-title">{title}</h2>
-        <h2 className="card-price">$ {price}</h2>
-      </div>
-      <p className="article mb">{article}</p>
-      <button className="btn mb" onClick={() => setShow(!show)}>
-        Not interested
-      </button>
-    </div>
+    <>
+      {show ? (
+        <div className="card">
+          <img src={img} alt="" />
+          <div className="card-name">
+            <h2 className="card-title">{title}</h2>
+            <h2 className="card-price">$ {price}</h2>
+          </div>
+
+          {article.length > 300 ? (
+            <p className="article mb">
+              {text}
+              {isMore ? (
+                <button
+                  className="readmore-btn"
+                  onClick={() => {
+                    setText(article);
+                    setIsMore(!isMore);
+                  }}
+                >
+                  Read more
+                </button>
+              ) : (
+                <button
+                  className="readmore-btn"
+                  onClick={() => {
+                    setText(article.substring(0, 201) + "...");
+                    setIsMore(!isMore);
+                  }}
+                >
+                  See less
+                </button>
+              )}
+            </p>
+          ) : (
+            console.log("hi")
+          )}
+          <button className="btn mb" onClick={() => setShow(!show)}>
+            Not interested
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 
