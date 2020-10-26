e81afa3cd12dad0f951485009030d42883c0a93d
Asfian99 <mk.asfian@gmail.com>
2020-10-26 22:27:54 +0700

    finishing terakhir

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import Card from "./Card";
 import { datas } from "./datas";
 
-// const url = "./datas.json";
-
 function App() {
   const [isLoading, setIsLoading] = React.useState(true);
   // const [datas, setDatas] = React.useState(url);
@@ -16,24 +14,10 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
-  // React.useEffect(() => {
-  //   fetch(url)
-  //     .then((resp) => {
-  //       if (resp.status >= 200 && resp.status <= 299) {
-  //         console.log("hello");
-  //         setDatas(resp);
-  //         setIsLoading(false);
-  //       } else {
-  //         setIsLoading(true);
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   if (isLoading) {
     return (
       <div>
-        <h1>Loading...</h1>
+        <h1 className="title">Loading...</h1>
       </div>
     );
   }
